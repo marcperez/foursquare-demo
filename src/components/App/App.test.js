5f30e1c27d8ca4e34072a5237f496b87ef06b3fe
Marc Perez <marcperez2000@gmail.com>
2018-03-01 22:43:41 +0100

    Fixes App test

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -2,6 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+const mockGeolocation = {
+  getCurrentPosition: jest.fn(),
+  watchPosition: jest.fn()
+};
+
+global.navigator.geolocation = mockGeolocation;
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
